ec11c043e53d25ad01a59612b6a5a87c27de3a38
SACHIN <sachinbadalaudr@gmail.com>
2017-08-30 09:19:11 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+	articles = [
+	{
+		'id':1,
+		'title':'Article One',
+		'body':'jsashdjhjashdjhfjdshafjhdsajfhjksdahfjksadhfljkhasd',
+		'author':'sachin badala',
+		'created_date':'09-12-2017'
+	},
+	{
+		'id':1,
+		'title':'Article One',
+		'body':'jsashdjhjashdjhfjdshafjhdsajfhjksdahfjksadhfljkhasd',
+		'author':'sachin badala',
+		'created_date':'09-12-2017'
+	},
+	
+	]
+	return articles
